d764df601e2eb78ba381227ee29fb1bed6233b18
Claudio Matheus <claudiomatheus055@gmail.com>
2024-08-20 10:11:16 -0300

    ADD: VALIDAÇÕES DAS TABELAS DO BANCO DE DADO

---
+++ ListaDeTarefasASPNET/Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace ListaDeTarefasASPNET.Models
+{
+    public class Categoria
+    {
+        public string CategoriaId { get; set; }
+        public string Nome { get; set; }
+    }
+}
